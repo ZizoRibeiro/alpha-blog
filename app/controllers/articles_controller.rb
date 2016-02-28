37197729bbd5374532abc58ac58ac6a171678bcc
Zizo Ribeiro <zizo.ribeiro@gmail.com>
2016-02-27 18:48:01 -0800

    Finished style errors and partials

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,23 +5,17 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
-
   def new
     @article = Article.new
-
   end
 
   def edit
- 
-
   end
 
-
   def create
-    
     @article = Article.new(article_params)
     if @article.save
-      flash[:notice] = "Article was successfully created"
+			flash[:success] = "Article was successfully created"
       redirect_to article_path(@article)
     else
       render 'new' 
@@ -29,9 +23,8 @@ class ArticlesController < ApplicationController
   end
 
     def update
- 
       if @article.update(article_params)
-        flash[:notice] = "Article was successfully updated"
+				flash[:success] = "Article was successfully updated"
         redirect_to article_path(@article)
       else
         render 'edit'
@@ -43,13 +36,10 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
- 
     @article.destroy
-    flash[:notice] = "Article was successfully deleted"
+		flash[:danger] = "Article was successfully deleted"
     redirect_to articles_path
   end
-
-
 
 
   private
